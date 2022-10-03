9eb8fea1996135e4cc50f5f3f3852da7f505fa33
fajarfadillahh <fajaragustian979@gmail.com>
2022-10-03 20:14:15 +0700

    home section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: "class",
@@ -8,6 +10,16 @@ module.exports = {
       md: "767px",
       lg: "968px",
       xl: "1024px",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: { ...colors.emerald, 500: "#5bb5a2" },
+      indigo: colors.indigo,
+      yellow: colors.yellow,
     },
     fontFamily: {
       body: ["Outfit", "sans-serif"],
